ee892284323070b6fbe34cfff56abc043b7d2cf7
Bellfalasch <bellfalasch@gmail.com>
2015-04-11 15:33:26 +0200

    Redesigned the Select page

--- _admin/migrate-select.php
+++ _admin/migrate-select.php
@@ -1,6 +1,6 @@
 <?php
 	/* Set up template variables */
-	$PAGE_name  = 'Select';
+	$PAGE_name  = 'Step 0';
 	$PAGE_title = 'Admin/' . $PAGE_name;
 	$PAGE_desc = 'start your migration journey';
 ?>
@@ -10,40 +10,59 @@
 
 	<div class="row">
 		<div class="span7">
-
 			<p>
-				Select which of your projects you wanna work on:
+				Select which of your projects you wanna work on in the migration process.
 			</p>
-
-			<?php
-				$result = db_getSites();
-				
-				if (!is_null($result))
-				{
-					echo "<ul>";
-					while ( $row = $result->fetch_object() )
-					{
-						if ($row->id == $PAGE_siteid)
-							$selected = " style=\"background-color:#ebebeb;\"";
-						else
-							$selected = "";
-
-						echo "<li" . $selected . "><a href=\"" . $SYS_pageself . "?project=" . $row->id . "\">" . $row->name . "</a> (Step: <strong>" . $row->step . "</strong>)<br /><em>" . $row->url . "</em></li>";
-					}
-					echo "</ul>";
-				}
-				else
-				{
-					echo "<p>No projects found (<a href=\"" . $SYS_pageroot . "project.php\">create one</a>!)</p>";
-				}
-			?>
-
 		</div>
 
 		<div class="span4 offset1">
 
 			<h4>Need more projects?</h4>
 			<p>Just go to the <a href="<?= $SYS_pageroot ?>project.php">Projects-page</a> and create some more then =)</p>
+
+		</div>
+	</div>
+
+	<p>&nbsp;</p>
+
+	<div class="row">
+		<div class="span12">
+			<ul class="thumbnails">
+
+				<?php
+					$result = db_getSites();
+
+					if (!is_null($result))
+					{
+						while ( $row = $result->fetch_object() )
+						{
+
+				?>
+
+					<li class="span4">
+						<div class="thumbnail">
+							<h3>
+								<a href="<?= $SYS_pageself ?>?project=<?= $row->id ?>"><?= $row->name ?></a>
+							</h3>
+							<em><?= $row->url ?></em>
+							<p>
+								Step: <span class="badge badge-inverse"><?= $row->step ?></span><br />
+								Pages: <span class="badge badge-inverse"><?= $row->pages ?></span>
+							</p>
+						</div>
+					</li>
+
+				<?php
+
+						}
+					}
+					else
+					{
+						echo "<p>No projects found (<a href=\"" . $SYS_pageroot . "project.php\">create one</a>!)</p>";
+					}
+				?>
+
+			</ul>
 
 		</div>
 	</div>
