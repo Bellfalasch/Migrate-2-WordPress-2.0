463590ff571973c5ed15655df503292a11158396
Bellfalasch <bellfalasch@gmail.com>
2015-04-11 16:35:02 +0200

    Added bootstrap responsive and JS

--- _admin/migrate-step7.php
+++ _admin/migrate-step7.php
@@ -129,7 +129,7 @@
 			if ($split_id > 0) {
 				echo "<td>-</td>";
 			} else {
-				echo "<td><a href=\"" . $SYS_pageself . "?split=" . $row->id . "\" class=\"btn btn-mini btn-primary\">Edit HTML</a></td>";
+				echo "<td><a href=\"" . $SYS_pageself . "?split=" . $row->id . "\" class=\"btn btn-mini btn-primary\" data-toggle=\"modal\" data-target=\"#myModal\">Edit HTML</a></td>";
 			}
 
 			$page = $row->page;
@@ -172,6 +172,21 @@
 ?>
 			</tbody>
 		</table>
+
+		<!-- Modal -->
+		<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+				<h3 id="myModalLabel">Modal header</h3>
+			</div>
+			<div class="modal-body">
+				<p>One fine body…</p>
+			</div>
+			<div class="modal-footer">
+				<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+				<button class="btn btn-primary">Save changes</button>
+			</div>
+		</div>
 
 <?php
 
