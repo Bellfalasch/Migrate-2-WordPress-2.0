dbe64d9ab36b259d651f5810d9dd59bf4a1c2379
Bellfalasch <bellfalasch@gmail.com>
2018-02-22 21:54:42 +0100

    Add instructions

--- config-example.php
+++ config-example.php
@@ -1,4 +1,5 @@
 <?php
+  // Rename this file to "config.php" and update these settings:
   DEFINE('DB_USER', 'xxx');			// Username for database
   DEFINE('DB_PASS', 'xxx');			// Password for database
   DEFINE('DB_HOST', 'xxx');	    // Server for database
