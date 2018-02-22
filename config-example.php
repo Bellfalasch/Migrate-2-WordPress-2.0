36140afed3783c8c833ad6142ff7b90c0f0e361d
Bellfalasch <bellfalasch@gmail.com>
2018-02-22 21:49:06 +0100

    Create config-file for settings instead

---
+++ config-example.php
@@ -0,0 +1,6 @@
+<?php
+  DEFINE('DB_USER', 'xxx');			// Username for database
+  DEFINE('DB_PASS', 'xxx');			// Password for database
+  DEFINE('DB_HOST', 'xxx');	    // Server for database
+  DEFINE('DB_NAME', 'xxx');			// Name of database
+?>
